af09ef867a5f82fb253617e393bab5707f212281
MSarina B-Siacone <124050454+LGBTALBUM@users.noreply.github.com>
2026-05-26 18:01:47 +0800

    Add lastmod and XML escaping to sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,8 +2,16 @@ import { getSortedReleases } from '../data/music';
 import { site } from '../data/content';
 
 const staticPages = ['/', '/music/', '/works/', '/blog/', '/links/', '/about/', '/contact/'];
+const lastmod = new Date().toISOString().slice(0, 10);
 
 const toAbsoluteUrl = (path: string) => new URL(path.replace(/^\/+/, ''), site.url).toString();
+const escapeXml = (value: string) =>
+  value
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
 
 export async function GET() {
   const releasePages = getSortedReleases().map((release) => `/music/${release.slug}/`);
@@ -11,7 +19,7 @@ export async function GET() {
 
   const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
     .map(
-      (url) => `  <url>\n    <loc>${toAbsoluteUrl(url)}</loc>\n  </url>`
+      (url) => `  <url>\n    <loc>${escapeXml(toAbsoluteUrl(url))}</loc>\n    <lastmod>${lastmod}</lastmod>\n  </url>`
     )
     .join('\n')}\n</urlset>\n`;
 
