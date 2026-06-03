b47f712560a043f9dfb9ad4d1d60953ebe423cd7
MSarina B-Siacone <124050454+LGBTALBUM@users.noreply.github.com>
2026-06-03 11:31:01 +0800

    feat: add friends data

---
+++ src/data/friends.ts
@@ -0,0 +1,38 @@
+export type FriendLink = {
+  name: string;
+  href: string;
+  note?: string;
+};
+
+export const friends: FriendLink[] = [
+  {
+    name: 'Estela ad Astra',
+    href: 'https://estela.moe/',
+    note: 'Friend site'
+  },
+  {
+    name: 'Clansty',
+    href: 'https://nyac.at/',
+    note: 'Friend site'
+  },
+  {
+    name: 'Leohearts',
+    href: 'https://leohearts.com/',
+    note: 'Friend site'
+  },
+  {
+    name: 'Menci',
+    href: 'https://men.ci/',
+    note: 'Friend site'
+  },
+  {
+    name: 'Maplef',
+    href: 'https://maplef.top/',
+    note: 'Friend site'
+  },
+  {
+    name: 'Kiritake Kumi',
+    href: 'https://niconiconi.us/',
+    note: 'Friend site'
+  }
+];
